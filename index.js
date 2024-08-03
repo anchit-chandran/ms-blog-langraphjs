c518da48b55528296389d2140e28c58244b5547e
anchit-chandran <anchit97123@gmail.com>
2024-08-03 14:36:04 +0100

    update with nodeaemon
    Signed-off-by: anchit-chandran <anchit97123@gmail.com>

--- index.js
+++ index.js
@@ -1,19 +1,48 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 // Import the 'express' module along with 'Request' and 'Response' types from express
 const express_1 = __importDefault(require("express"));
+const langgraph_1 = require("@langchain/langgraph");
 // Create an Express application
 const app = (0, express_1.default)();
 // Specify the port number for the server
 const port = 3000;
-// Define a route for the root path ('/')
-app.get("/", (req, res) => {
-    // Send a response to the client
+app.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    // State
+    const graphState = {};
+    // A node that says hello
+    function sayHello(state) {
+        console.log(`From the 'sayHello' node: Hello world!`);
+        return {};
+    }
+    // Initialize the LangGraph
+    const graphBuilder = new langgraph_1.StateGraph({ channels: graphState })
+        // Add our 'sayHello' node to the graph
+        .addNode("sayHello", sayHello)
+        // Add the edges to the graph
+        .addEdge(langgraph_1.START, "sayHello")
+        .addEdge("sayHello", langgraph_1.END);
+    // Compile the graph
+    const graph = graphBuilder.compile();
+    // Execute the graph!
+    const result = yield graph.invoke("sayHello");
+    console.log('\n=====START======');
+    console.log("Graph result: ", result);
+    console.log('\n=====END======');
     res.send("Hello, TypeScript + Node.js + Express!");
-});
+}));
 // Start the server and listen on the specified port
 app.listen(port, () => {
     // Log a message when the server is successfully running
--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 // Import the 'express' module along with 'Request' and 'Response' types from express
 import express, { Request, Response } from "express";
+import { StateGraph, START, END, StateGraphArgs } from "@langchain/langgraph";
 
 // Create an Express application
 const app = express();
@@ -7,9 +8,37 @@ const app = express();
 // Specify the port number for the server
 const port: number = 3000;
 
-// Define a route for the root path ('/')
-app.get("/", (req: Request, res: Response) => {
-  // Send a response to the client
+app.get("/", async (req: Request, res: Response) => {
+  // State type
+  interface IState {}
+
+  // State
+  const graphState: StateGraphArgs<IState>["channels"] = {};
+
+  // A node that says hello
+  function sayHello(state: IState) {
+    console.log(`From the 'sayHello' node: Hello world!`);
+    return {};
+  }
+
+  // Initialize the LangGraph
+  const graphBuilder = new StateGraph({ channels: graphState })
+    // Add our 'sayHello' node to the graph
+    .addNode("sayHello", sayHello)
+    // Add the edges to the graph
+    .addEdge(START, "sayHello")
+    .addEdge("sayHello", END);
+
+  // Compile the graph
+  const graph = graphBuilder.compile();
+
+  // Execute the graph!
+  const result = await graph.invoke("sayHello");
+
+  console.log('\n=====START======')
+  console.log("Graph result: ", result);
+  console.log('\n=====END======')
+
   res.send("Hello, TypeScript + Node.js + Express!");
 });
 
